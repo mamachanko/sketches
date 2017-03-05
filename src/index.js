8043113c8fa765fbef8ea4d79d1248c35257180b
mamachanko <sugardubz@gmail.com>
2017-03-05 09:47:32 +0100

    avoid Object.values

--- src/index.js
+++ src/index.js
@@ -3,27 +3,26 @@ import './googleAnalytics';
 import * as sketches from './sketches';
 import Navigo from 'navigo';
 
-let routes = {};
-Object.values(sketches)
-    .map((sketch, i) =>
-        routes[`sketches/0${i + 1}`] = () => sketch()
-    );
+const sketchesArray = Object.keys(sketches).map((k) => {
+    return sketches[k]
+});
 
-routes['sketches/any'] = () => {
-    const randomItem = (array) => array[Math.floor(Math.random() * array.length)];
-    randomItem(Object.values(sketches))();
-};
-
-routes['sketches/latest'] = () => {
-    const last = (object) => [...Object.values(object)].pop();
-    last(sketches)();
-};
-
-routes['sketches'] = () => {
+const lastItem = array => [...array].pop();
+const randomItem = array => array[Math.floor(Math.random() * array.length)];
+const allItemsAsHtml = (routes) => {
     document.querySelector('#app').innerHTML = Object.keys(routes)
-        .map((key, _) => `<div><a href="${key}">${key}</a></div>`).join(' ');
+        .map((key, _) => `<div><a href="${key}">${key}</a></div>`)
+        .join(' ');
 };
 
+let routes = {};
+sketchesArray.map((sketch, i) =>
+    routes[`sketches/0${i + 1}`] = () => sketch()
+);
+
+routes['sketches/any'] = () => randomItem(sketchesArray)();
+routes['sketches/latest'] = () => lastItem(sketchesArray)();
+routes['sketches'] = () => allItemsAsHtml(routes);
 routes['*'] = routes['sketches/latest'];
 
 new Navigo().on(routes).resolve();
